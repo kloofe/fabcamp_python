c9d368983fab0b30be81bff151695b471257f65a
Andrew Cassidy <drewcassidy@me.com>
2016-08-11 10:21:57 -0700

    Whitespace changes to comply with PEP8

--- servo1.py
+++ servo1.py
@@ -9,13 +9,12 @@ GPIO.setup(???, GPIO.OUT)        # What is the gpio pin number we are using?
 servo = GPIO.PWM(???, 100)       # Initialize servo on pwmPin 100Hz frequency. What is the gpio pin number we are using?
 servo.start(5)
 
-# What range do we want if we want the the handle to rotate 180 degrees? Remember it's good to have some offset
-for angle in range(???,???): 
+# What range do we want if we want the the handle to rotate 180 degrees?
+# Remember it's good to have some offset
+for angle in range(???, ???):
     duty = float(angle) / 10.0 + 2.5
     servo.ChangeDutyCycle(duty)
-    time.sleep(0.01)	
-	
-	
-servo.stop()        # stop servo
-GPIO.cleanup()      # cleanup all GPIO
+    time.sleep(0.01)
 
+servo.stop()    # stop servo
+GPIO.cleanup()  # cleanup all GPIO
